2e48852875de3ee9344653da220ce3583eb254f8
lsh <rrgks@naver.com>
2022-04-29 12:08:56 +0900

    Add: makeRespnse test코드 작성 #3

---
+++ app/tests/utils/makeResponse.spec.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const makeResponse = require('../../src/util/makeResponse');
+
+describe('응답 데이터 생성 로직', () => {
+  it('200번대 moreInfo 없음', () => {
+    const response = makeResponse(200, '클라이언트 메시지');
+
+    expect(response).toEqual({
+      success: true,
+      msg: '클라이언트 메시지',
+      status: 200,
+    });
+  });
+
+  it('200번대 moreInfo 있음', () => {
+    const moreInfo = { temp1: 'temp1', temp2: 'temp2' };
+    const response = makeResponse(200, '클라이언트 메시지', moreInfo);
+
+    expect(response).toEqual({
+      success: true,
+      msg: '클라이언트 메시지',
+      status: 200,
+      temp1: moreInfo.temp1,
+      temp2: moreInfo.temp2,
+    });
+  });
+
+  it('300번대', () => {
+    const response = makeResponse(300, '클라이언트 메시지');
+
+    expect(response).toEqual({
+      success: true,
+      msg: '클라이언트 메시지',
+      status: 300,
+    });
+  });
+
+  it('400번대', () => {
+    const response = makeResponse(400, '클라이언트 메시지');
+
+    expect(response).toEqual({
+      success: false,
+      msg: '클라이언트 메시지',
+      status: 400,
+    });
+  });
+});
