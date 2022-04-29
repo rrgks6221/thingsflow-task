3cf774655a546fe537648246ba462f3ccda78c42
lsh <rrgks@naver.com>
2022-04-29 11:56:05 +0900

    Modify: Error.ctrl clientMsg 변경 #3

--- app/src/util/Error.js
+++ app/src/util/Error.js
@@ -4,7 +4,7 @@ class Error {
   static ctrl(err) {
     return {
       success: false,
-      clientMsg: '서버 에러입니다. 서버 개발자에게 문의해주세요.',
+      clientMsg: err.message || 'Internal Server Error',
       err: err.stack,
       status: err.status || 500,
     };
